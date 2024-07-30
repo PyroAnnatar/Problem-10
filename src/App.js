f9564b080fb9e1a416c75b651480ee0eff96633a
PyroAnnatar <pyroannatar@gmail.com>
2024-07-31 01:33:48 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import { EllipsisVerticalIcon } from "@heroicons/react/20/solid";
 
 // Card'a tek tek proplar ekleyin, ardından bunları kullanmak için her birini yeniden yapılandırın
-function Card({ username }) {
+function Card({ username, content, date, img }) {
   return (
     <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
       <div class="bg-white sm:rounded-lg sm:shadow">
         <div className="px-4 py-5 sm:px-6">
           <div className="flex space-x-3">
             <div className="flex-shrink-0">
-              <img
-                className="h-10 w-10 rounded-full"
-                src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                alt=""
-              />
+              <img className="h-10 w-10 rounded-full" src={img} alt="" />
             </div>
             <div className="min-w-0 flex-1">
               <p className="text-sm font-semibold text-gray-900">
@@ -22,7 +18,7 @@ function Card({ username }) {
               </p>
               <p className="text-sm text-gray-500">
                 <a href="#" className="hover:underline">
-                  9 Aralık saat 11:43
+                  {date}
                 </a>
               </p>
             </div>
@@ -30,14 +26,7 @@ function Card({ username }) {
           </div>
         </div>
         <div class="px-4 pb-5 opacity-25 sm:p-6 sm:pt-0">
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
-            impedit sapiente recusandae iusto officiis dolor? Laborum quibusdam
-            quam, quidem vel assumenda repellat inventore sint nesciunt, ullam
-            asperiores magnam placeat eveniet. Aliquam voluptatibus assumenda
-            distinctio veniam quam tempora modi aperiam nemo voluptate
-            reprehenderit quidem, nisi vero est.
-          </p>
+          <p>{content}</p>
         </div>
       </div>
     </div>
@@ -62,7 +51,17 @@ function CardButton() {
 export default function App() {
   return (
     <div className="bg-gray-100">
-      <Card username="Chelsea Hagon" />
+      <Card
+        username="Chelsea Hagon"
+        content="            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo
+            impedit sapiente recusandae iusto officiis dolor? Laborum quibusdam
+            quam, quidem vel assumenda repellat inventore sint nesciunt, ullam
+            asperiores magnam placeat eveniet. Aliquam voluptatibus assumenda
+            distinctio veniam quam tempora modi aperiam nemo voluptate
+            reprehenderit quidem, nisi vero est."
+        date="9 Aralık saat 11:43"
+        img="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+      />
     </div>
   );
 }
